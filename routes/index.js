2d1b04efab2662812f42246276dbdacbc80778a5
Elu Gonzalez <elugon10@gmail.com>
2022-09-06 19:32:57 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const ErrorResponse = require('../utils/error');
+const SneaksAPI = require('sneaks-api');
+const sneaks = new SneaksAPI();
 
 // @desc    GET all the projects
 // @route   GET /api/v1/
@@ -7,5 +9,25 @@ const ErrorResponse = require('../utils/error');
 router.get('/', async (req, res, next) => {
   res.send('REST API')
 });
+
+// @desc    GET all the projects
+// @route   GET /api/v1/
+// @access  Public
+router.get('/shoes', async (req, res, next) => {
+  try {
+    const shoes = await sneaks.getProducts("Yeezy Cinder", 10, function(err, products){
+      console.log(products)
+  })
+    if (!shoes) {
+      res.status(404).json({ response: 'Shoes not found' });
+    } else {
+      res.status(200).json({ data: shoes })
+    }
+  } catch (error) {
+    next(error);
+  }
+
+});
+
 
 module.exports = router;
