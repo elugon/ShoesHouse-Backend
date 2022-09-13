df24fe6c469f53d3b3167e002f2f2cdc8faa24a7
Elu Gonzalez <elugon10@gmail.com>
2022-09-13 21:27:15 +0200

    comments improved

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const errorHandler = require('./middlewares/errorHandler');
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 const allShoes=require('./routes/shoes')
+const allComments=require('./routes/comments')
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/shoes', allShoes)
+app.use('/api/v1/comments', allComments)
 app.use(errorHandler);
 
 // catch 404 and forward to error handler
